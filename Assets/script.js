c8295282c2a5f6d361114b3eb8a42cdc66ac3bd5
Mike Coletta <coletta.mike@gmail.com>
2021-01-22 19:31:25 -0800

    Initial commit

---
+++ Assets/script.js
@@ -0,0 +1,11 @@
+//function that displays the current time and date
+
+//make HTML for the time blocks that represent 9am - 5pm
+
+//have the time blocks show past present or future. Will need a current time variable
+
+//time blocks are clickable and can be written in.
+
+//time block content is saved to local storage
+
+//localStorage.getItem to load appointments into correct time blocks.
