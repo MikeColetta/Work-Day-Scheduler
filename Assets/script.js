d83c419f1cefef06cc85e44eb52a517dc2557303
Mike Coletta <coletta.mike@gmail.com>
2021-01-23 15:12:02 -0800

    Added time to script and HTML

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,10 @@
 //function that displays the current time and date using Luxon.
 
+const currentTime = moment()
+console.log(currentTime)
+
+$("#currentDay").text(currentTime)
+
 //make HTML for the time blocks that represent 9am - 5pm
 
 //have the time blocks show past present or future. Will need a current time variable
